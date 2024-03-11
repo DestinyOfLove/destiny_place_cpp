00433d0db9d079fe172a3f2908a7d0c8ac4e8c24
灰道 <sulan.sl@alibaba-inc.com>
2024-03-11 22:37:03 +0800

    fix: 修改股票最佳购买和售出时机

--- leetcode/best_time_to_buy_and_sell_stock.cpp
+++ leetcode/best_time_to_buy_and_sell_stock.cpp
@@ -5,29 +5,6 @@
 using namespace std;
 
 /**
- * 双指针
- */
-int maxProfitDoublePointer(vector<int> &prices) {
-    int buy = 0; // 买入指针，表示买入时机的索引
-    int sell = 1; // 卖出指针，表示卖出时机的索引
-    int max_profit = 0; // 最大利润初始化为0
-
-    while (sell < prices.size()) {
-        if (prices[buy] < prices[sell]) {
-            // 如果当前卖出价比买入价高，计算此次交易的利润，并更新最大利润
-            int profit = prices[sell] - prices[buy];
-            max_profit = max(max_profit, profit);
-        } else {
-            // 如果遇到更低的买入价，则更新买入指针
-            buy = sell;
-        }
-        sell++; // 卖出指针始终向前移动
-    }
-
-    return max_profit;
-}
-
-/**
  * 直接遍历
  */
 int maxProfit(vector<int> &prices) {
