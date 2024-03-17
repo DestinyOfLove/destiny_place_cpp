c43d7cff5b0c2356b6393b1777bc14c4e3fbb3c6
灰道 <sulan.sl@alibaba-inc.com>
2024-03-18 01:08:49 +0800

    feat: 添加算法题，找到有序数组中的最小值，更新解法

--- leetcode/find_minimum_in_rotated_sorted_array.cpp
+++ leetcode/find_minimum_in_rotated_sorted_array.cpp
@@ -4,14 +4,22 @@
 class Solution {
 public:
     int findMin(std::vector<int> &nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
         int left = 0;
+        int max_right_num = nums[nums.size() - 1];
         int right = nums.size() - 1;
-        while (left < right) {
+        /*
+         * 1. 找下界，返回left；
+         * 2. 注意是否添加==判断。如果不添加，则会走left = mid + 1，意味着当left == mid == right（找到目标值）时，left会改变，而返回的值正是nums[left]。导致答案错误
+         */
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
+            if (nums[mid] <= max_right_num) {
+                right = mid - 1;
             } else {
-                right = mid;
+                left = mid + 1;
             }
         }
         return nums[left];
@@ -20,7 +28,7 @@ public:
 
 int main() {
     Solution solution;
-    std::vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    std::vector<int> nums = {2, 1};
     int min = solution.findMin(nums);
     std::cout << "The minimum number in the array is: " << min << std::endl;
     return 0;
