c80f55a15c84c6e65f9fb1580cf0d13fdfbf792d
灰道 <sulan.sl@alibaba-inc.com>
2024-03-18 00:39:50 +0800

    feat: 添加算法题，有序序列里的单个元素，更新解法

--- leetcode/single_element_in_a_sorted_array.cpp
+++ leetcode/single_element_in_a_sorted_array.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int singleNonDuplicate(vector<int> &nums) {
     int left = 0, right = nums.size() - 1;
-    while (left < right) {
+    /*
+     * 1. 满足条件的下界;
+     * 2. return left
+     */
+    while (left <= right) {
         int mid = left + (right - left) / 2;
         // 确保mid是偶数，如果为奇数则减一
         if (mid % 2 == 1) {
@@ -15,7 +19,7 @@ int singleNonDuplicate(vector<int> &nums) {
         if (nums[mid] == nums[mid + 1]) {
             left = mid + 2;  // 唯一元素在右侧
         } else {
-            right = mid;  // 唯一元素在左侧
+            right = mid - 1;  // 唯一元素在左侧
         }
     }
     return nums[left];
