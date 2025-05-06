3e0f5b502841b7fdc544247e55ec28b01680f1de
sulan.sl <sulan.sl@alibaba-inc.com>
2025-05-06 17:02:07 +0800

    feat(message_queue): 添加多种消息队列实现
    添加了LockFreeMQ, SPSC, RingMQMPMC 和 MPMC四种消息队列的实现。这些新加入的消息队列为系统提供了更多选择，以适应不同的并发场景需求。

---
+++ message_queue/LockFreeMQ.h
@@ -0,0 +1,100 @@
+#pragma once
+#include <atomic>
+
+namespace destiny {
+
+template <typename T>
+struct MQNode {
+    T* data;
+    std::atomic<MQNode*> next;
+
+    MQNode() : data(nullptr), next(nullptr) {}
+
+    MQNode(T* data) : data(data), next(nullptr) {}
+};
+
+template <typename T>
+class LockFreeMQ {
+public:
+    LockFreeMQ();
+    ~LockFreeMQ();
+
+    void push(const T& data);
+    T* pop();
+
+private:
+    std::atomic<MQNode<T>*> head_;
+    std::atomic<MQNode<T>*> tail_;
+};
+
+template <typename T>
+LockFreeMQ<T>::LockFreeMQ() {
+    MQNode<T>* dummy = new MQNode<T>();
+    head_.store(dummy);
+    tail_.store(dummy);
+}
+
+template <typename T>
+LockFreeMQ<T>::~LockFreeMQ() {
+    while (T* tmp = pop()) {
+        delete tmp;
+        tmp = nullptr;
+    }
+    delete head_.load();
+}
+
+template <typename T>
+void LockFreeMQ<T>::push(const T& data) {
+    T* new_data = new T(data);
+    MQNode<T>* new_node = new MQNode<T>(new_data);
+
+    while (true) {
+        MQNode<T>* old_tail = tail_.load();
+        MQNode<T>* old_tail_next = old_tail->next.load();
+        if (tail_.load() == old_tail) {
+            if (old_tail_next == nullptr) {
+                if (old_tail->next.compare_exchange_weak(old_tail_next, new_node)) {
+                    tail_.compare_exchange_strong(old_tail, new_node);
+                    return;
+                }
+                tail_.compare_exchange_weak(old_tail, old_tail_next);
+            } else {
+                tail_.compare_exchange_weak(old_tail, old_tail_next);
+            }
+        }
+    }
+}
+
+template <typename T>
+T* LockFreeMQ<T>::pop() {
+    while (true) {
+        MQNode<T>* old_head = head_.load();
+        MQNode<T>* old_head_next = old_head->next.load();
+        MQNode<T>* tail = tail_.load();
+
+        // 尝试插入，更新 head为新的 dummy head
+        // 判断队列是否为空，为空返回 nullptr
+        // 不为空，则更新 head
+        if (head_.load() == old_head) {
+            if (old_head == tail) {
+                // 存在两种可能，1. 为空；2. 只有一个元素，但是 next没来得及更新
+                if (old_head_next == nullptr) {
+                    return nullptr;
+                }
+                // 辅助推进，加速
+                tail_.compare_exchange_weak(tail, old_head_next);
+            } else {
+                // 不为空。尝试更新 head
+                if (head_.compare_exchange_weak(old_head, old_head_next)) {
+                    // 成功了，需要返回结果，并将 next处理为 dummy
+                    T* res = old_head_next->data;
+                    old_head_next->data = nullptr;
+                    delete old_head;
+                    return res;
+                }
+            }
+        }
+    }
+}
+
+}  // namespace destiny
