8b548833b6c053c9e9582c4206cc7063dff02be7
destiny <destiny@destinys-Mac-mini.local>
2025-08-16 17:30:27 +0800

    fix: correct pop function return value logic in spsc_queue.h
    - Fixed pop() function returning T instead of bool
    - Added missing newline at end of file
    - Applied clang-format formatting

---
+++ message_queue/spsc_queue.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <queue>
+
+namespace destiny {
+template <typename T>
+
+// 单线程 queue，非阻塞模式
+class SPSC_Queue {
+public:
+    bool push(const T& t) {
+        msg_q_.push(t);
+        return true;
+    }
+
+    bool pop(T& t) {
+        if (msg_q_.empty()) {
+            return false;
+        }
+        t = msg_q_.front();
+        msg_q_.pop();
+        return true;
+    }
+
+private:
+    std::queue<T> msg_q_;
+};
+}  // namespace destiny
